adbba81d7fd34170d19fdd0a37cddc376101065d
Yuen Boon Yee <yuen_boon_yee@sereneit.com>
2015-09-11 10:36:44 +1000

    Refactored to remove duplicated code.

--- AccessibilityDemo2/AccessibleGraph.m
+++ AccessibilityDemo2/AccessibleGraph.m
@@ -31,7 +31,7 @@
 /*!
  @brief  Used as a data source for the UIAccessibilityContainer protocol.
  @discussion Create and configure a UIAccessibilityElement object for each of the elements in our custom view and store them in an array.
- @return <#return value description#>
+ @return An array of UIAccessibleElements.
  */
 - (NSMutableArray *)accessibleElements
 {
@@ -43,34 +43,31 @@
     _accessibleElements = [[NSMutableArray alloc] init];
     
     //Create accessible elements for each slice of the graph.
-    //Left
-    UIAccessibilityElement *graphElementLeft = [[UIAccessibilityElement alloc] initWithAccessibilityContainer:self];
-    //The accessibility frame must be specified in screen coordinates
-    graphElementLeft.accessibilityFrame = [self convertRect:self.graphSegmentLeft.frame
-                                                                      toView:nil];
-    graphElementLeft.accessibilityLabel = NSLocalizedString(@"Steve Jobs unveils first iPhone. No dramatic surge in Apple share price until about 1 year later.", nil);
-    graphElementLeft.accessibilityTraits = UIAccessibilityTraitStaticText;
-    [_accessibleElements addObject:graphElementLeft];
-    
-    //Middle
-    UIAccessibilityElement *graphElementMiddle = [[UIAccessibilityElement alloc] initWithAccessibilityContainer:self];
-    graphElementMiddle.accessibilityFrame = [self convertRect:self.graphSegmentMiddle.frame
-                                                                        toView:nil];
-    graphElementMiddle.accessibilityLabel = NSLocalizedString(@"Share price goes up and down for no apparent reason.", nil);
-    graphElementMiddle.accessibilityTraits = UIAccessibilityTraitStaticText;
-    [_accessibleElements addObject:graphElementMiddle];
-    
-    //Right
-    UIAccessibilityElement *graphElementRight = [[UIAccessibilityElement alloc] initWithAccessibilityContainer:self];
-    graphElementRight.accessibilityFrame = [self convertRect:self.graphSegmentRight.frame
-                                                                       toView:nil];
-    graphElementRight.accessibilityLabel = NSLocalizedString(@"Share price goes nuts.", nil);
-    graphElementRight.accessibilityTraits = UIAccessibilityTraitStaticText;
-    [_accessibleElements addObject:graphElementRight];
-    
-    
-    
+    [_accessibleElements addObject:[self createAccessibilityElementForGraphSegmentView:self.graphSegmentLeft
+                                                                             withLabel:@"Steve Jobs unveils first iPhone. No dramatic surge in Apple share price until about 1 year later."]];
+
+    [_accessibleElements addObject:[self createAccessibilityElementForGraphSegmentView:self.graphSegmentMiddle
+                                                                             withLabel:@"Share price goes up and down for no apparent reason."]];
+
+    [_accessibleElements addObject:[self createAccessibilityElementForGraphSegmentView:self.graphSegmentRight
+                                                                             withLabel:@"Share price goes nuts."]];
     return _accessibleElements;
+}
+
+/*!
+ @brief  Creates an UIAccessibilityElement.
+ @param view								The graph segment UIView.
+ @param accessibilityLabel	The accessibility label.
+ @return A UIAccessibilityElement.
+ */
+-(UIAccessibilityElement *)createAccessibilityElementForGraphSegmentView: (UIView *)view
+                                                   withLabel:(NSString *)accessibilityLabel{
+    UIAccessibilityElement *accessibilityElement = [[UIAccessibilityElement alloc] initWithAccessibilityContainer:self];
+    accessibilityElement.accessibilityFrame = [self convertRect:view.frame
+                                                      toView:nil];
+    accessibilityElement.accessibilityLabel = NSLocalizedString(accessibilityLabel, nil);
+    accessibilityElement.accessibilityTraits = UIAccessibilityTraitStaticText;
+    return accessibilityElement;
 }
 
 #pragma mark - UIAccessibilityProtocol conformance
