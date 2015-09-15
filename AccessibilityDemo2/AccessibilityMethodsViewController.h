73fd6751fe42f4b292af56f28aaf62f2e4b0d283
Yuen Boon Yee <yuen_boon_yee@sereneit.com>
2015-09-16 07:54:40 +1000

    Added 'Accessibility Methods' view controller. Added tab icons to all view controllers.

---
+++ AccessibilityDemo2/AccessibilityMethodsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccessibilityMethodsViewController.h
+//  AccessibilityDemo2
+//
+//  Created by Serene IT on 16/09/2015.
+//  Copyright (c) 2015 Serene IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccessibilityMethodsViewController : UIViewController
+
+@end
---
+++ AccessibilityDemo2/AccessibilityMethodsViewController.m
@@ -0,0 +1,37 @@
+//
+//  AccessibilityMethodsViewController.m
+//  AccessibilityDemo2
+//
+//  Created by Serene IT on 16/09/2015.
+//  Copyright (c) 2015 Serene IT. All rights reserved.
+//
+
+#import "AccessibilityMethodsViewController.h"
+
+@interface AccessibilityMethodsViewController ()
+
+@end
+
+@implementation AccessibilityMethodsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
