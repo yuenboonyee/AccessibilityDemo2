544f01379f1e226f76c037e7050cf38d47fd27e6
Yuen Boon Yee <yuen_boon_yee@sereneit.com>
2015-09-16 09:03:15 +1000

    Added demo on UIAccessibility notifications.

--- AccessibilityDemo2/AccessibilityMethodsViewController.m
+++ AccessibilityDemo2/AccessibilityMethodsViewController.m
@@ -9,6 +9,19 @@
 #import "AccessibilityMethodsViewController.h"
 
 @interface AccessibilityMethodsViewController ()
+@property (weak, nonatomic) IBOutlet UISwitch *switchIsVoiceOverRunning;
+@property (weak, nonatomic) IBOutlet UISwitch *switchIsBoldTextEnabled;
+@property (weak, nonatomic) IBOutlet UISwitch *switchIsClosedCaptioningEnabled;
+@property (weak, nonatomic) IBOutlet UISwitch *switchDarkerSystemColorsEnabled;
+@property (weak, nonatomic) IBOutlet UISwitch *switchIsGrayscaleEnabled;
+@property (weak, nonatomic) IBOutlet UISwitch *switchIsGuidedAccessEnabled;
+@property (weak, nonatomic) IBOutlet UISwitch *switchIsInvertColorsEnabled;
+@property (weak, nonatomic) IBOutlet UISwitch *switchIsMonoAudioEnabled;
+@property (weak, nonatomic) IBOutlet UISwitch *switchIsReduceMotionEnabled;
+@property (weak, nonatomic) IBOutlet UISwitch *switchIsReduceTransparencyEnabled;
+@property (weak, nonatomic) IBOutlet UISwitch *switchIsSpeakScreenEnabled;
+@property (weak, nonatomic) IBOutlet UISwitch *switchIsSpeakSelectionEnabled;
+@property (weak, nonatomic) IBOutlet UISwitch *switchIsSwitchControlRunning;
 
 @end
 
@@ -17,6 +30,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self registerForAccessibilityNotifications];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,6 +38,9 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)viewDidAppear:(BOOL)animated{
+    [self updateSwitches];
+}
 /*
 #pragma mark - Navigation
 
@@ -34,4 +51,61 @@
 }
 */
 
+#pragma mark - Private Methods
+/*!
+ @brief  Registers to listen to accessibility notifications.
+ */
+-(void)registerForAccessibilityNotifications{
+    [self registerForAccessibilityNotification:UIAccessibilityBoldTextStatusDidChangeNotification];
+    [self registerForAccessibilityNotification:UIAccessibilityClosedCaptioningStatusDidChangeNotification];
+    [self registerForAccessibilityNotification:UIAccessibilityDarkerSystemColorsStatusDidChangeNotification];
+    [self registerForAccessibilityNotification:UIAccessibilityGrayscaleStatusDidChangeNotification];
+    [self registerForAccessibilityNotification:UIAccessibilityGuidedAccessStatusDidChangeNotification];
+    [self registerForAccessibilityNotification:UIAccessibilityInvertColorsStatusDidChangeNotification];
+//    [self registerForAccessibilityNotification:UIAccessibilityLayoutChangedNotification];
+    [self registerForAccessibilityNotification:UIAccessibilityMonoAudioStatusDidChangeNotification];
+//    [self registerForAccessibilityNotification:UIAccessibilityPageScrolledNotification];
+//    [self registerForAccessibilityNotification:UIAccessibilityPauseAssistiveTechnologyNotification];
+    [self registerForAccessibilityNotification:UIAccessibilityReduceMotionStatusDidChangeNotification];
+    [self registerForAccessibilityNotification:UIAccessibilityReduceTransparencyStatusDidChangeNotification];
+//    [self registerForAccessibilityNotification:UIAccessibilityResumeAssistiveTechnologyNotification];
+//    [self registerForAccessibilityNotification:UIAccessibilityScreenChangedNotification];
+    [self registerForAccessibilityNotification:UIAccessibilitySpeakScreenStatusDidChangeNotification];
+    [self registerForAccessibilityNotification:UIAccessibilitySpeakSelectionStatusDidChangeNotification];
+    [self registerForAccessibilityNotification:UIAccessibilitySwitchControlStatusDidChangeNotification];
+    [self registerForAccessibilityNotification:UIAccessibilityVoiceOverStatusChanged];
+    
+}
+
+/*!
+ @brief  Registers a single notification to update the switches.
+ @param notification	UIAccessibilityNotification name.
+ */
+-(void)registerForAccessibilityNotification:(NSString *)notification{
+    //Register a notification handler.
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(updateSwitches)
+                                                 name:notification
+                                               object:nil];
+}
+
+/*!
+ @brief  Checks the UIAccessibility status and updates the switches.
+ */
+-(void)updateSwitches{
+    self.switchIsVoiceOverRunning.on = UIAccessibilityIsVoiceOverRunning();
+    self.switchIsBoldTextEnabled.on = UIAccessibilityIsBoldTextEnabled();
+    self.switchIsClosedCaptioningEnabled.on = UIAccessibilityIsClosedCaptioningEnabled ();
+    self.switchDarkerSystemColorsEnabled.on = UIAccessibilityDarkerSystemColorsEnabled ();
+    self.switchIsGrayscaleEnabled.on = UIAccessibilityIsGrayscaleEnabled ();
+    self.switchIsGuidedAccessEnabled.on = UIAccessibilityIsGuidedAccessEnabled ();
+    self.switchIsInvertColorsEnabled.on = UIAccessibilityIsInvertColorsEnabled ();
+    self.switchIsMonoAudioEnabled.on = UIAccessibilityIsMonoAudioEnabled ();
+    self.switchIsReduceMotionEnabled.on = UIAccessibilityIsReduceMotionEnabled ();
+    self.switchIsReduceTransparencyEnabled.on = UIAccessibilityIsReduceTransparencyEnabled ();
+    self.switchIsSpeakScreenEnabled.on = UIAccessibilityIsSpeakScreenEnabled ();
+    self.switchIsSpeakSelectionEnabled.on = UIAccessibilityIsSpeakSelectionEnabled ();
+    self.switchIsSwitchControlRunning.on = UIAccessibilityIsSwitchControlRunning ();
+
+}
 @end
--- Readme.txt
+++ Readme.txt
@@ -1,7 +1,9 @@
 This demo illustrates how to:
 - make a form accessible, even with elements that are contained in a UIView subview.
 - make a graph accessible using the UIAccessibilityContainer informal protocol.
+- Listen to UIAccessibility notifications and query the UIAccessibility framework for the accessibility status.
 
 
 References:
 http://useyourloaf.com/blog/voiceover-accessibility.html
+http://useyourloaf.com/blog/detecting-voiceover-status-changes.html
